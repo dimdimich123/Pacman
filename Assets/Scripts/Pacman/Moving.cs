d80e6e5b5e2a54e75fa30713ef81358796396b77
dimdimich123 <frostm333@users.noreply.github.com>
2021-11-14 12:12:01 +0700

    Initial commit

---
+++ Assets/Scripts/Pacman/Moving.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    private float _speed = 0.5f;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        inputX *= Time.deltaTime * _speed;
+        inputY *= Time.deltaTime * _speed;
+
+        transform.position = new Vector2(transform.position.x + inputX, transform.position.y + inputY);
+        //transform.Translate(inputX, inputY, 0);
+    }
+}
